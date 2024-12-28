542e72e995f1b9141968ab50dc1d043839546058
NaruseNia <notshinsin0000@gmail.com>
2024-12-29 01:48:10 +0900

    Resource Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        black: "#201d21",
+      },
       fontFamily: {
         sans: [
           "Inter",
